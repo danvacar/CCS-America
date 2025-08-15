defa64720d2bfd537bc1a1ab51fc8754d9cef17b
Valentina <danvacar@espol.edu.ec>
2025-08-14 21:32:41 -0500

    Update web app title and add logo

--- ccs_app.py
+++ ccs_app.py
@@ -1,9 +1,10 @@
 # Import Python Libraries
-
 import pandas as pd
 import numpy as np
 import streamlit as st
 
+st.set_page_config(page_title="CCS América", page_icon="sources/icons/logo.png", layout="wide")
 
 # Insert icon of web app
-# icon = Image.open("uiir.png")
+# icon = Image.open("logo.png")
+
